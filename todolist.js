22975ea9c4baff756a1037ea8ef1e3b542f78504
Your Name <you@example.com>
2021-04-01 17:45:50 +0100

    newchanges

--- todolist.js
+++ todolist.js
@@ -1,33 +1,46 @@
+
 var input=document.getElementById("task");
+
+
 //create button to take task from todolist 
 var btn =document.createElement("button");
-//set function includng the code above
-btn.setAttribute('onclick', 'close()');
-btn.style.width="80px";
-btn.style.height="40px"
-// create span to put button inside him
-var spn=document.createElement("span");
-spn.appendChild(btn);
+
+
+// function for button add that add text of button Add 
 function add(){
  if(input.value ==''){
-     alert("please enter something")
+     alert("please enter something")  
  }else{
+    //  create a div elemnt for add p elemnt and span button inside him 
+    var div = document.createElement("div");
+    // give a class name for div
+    div.setAttribute("class","t")
+    // create an elemnt p 
      var para = document.createElement("p");
-     para.style.fontFamily="roboto";
-     para.style.fontSize="20px";
-     para.style.backgroundColor="#e3f2fd";   
-     para.style.marginTop="20px";
-     para.style.width="400px";
-     para.style.height="40px";
-    para.innerHTML =input.value;
+     para.innerHTML =input.value;
+     //create button to take task from todolist 
+    var btn =document.createElement("button");
+    // set a text for butto
+    btn.textContent="Close";
+    // set a click event button for delete an elemnt that have done with task .
+    btn.onclick = function (e) {
+               // event : e
+        // we need to get a button from event using target 
+        var button = e.target;
+        // from button we need to get the parent that have a class name task
+        button.closest(".t").remove();
+         
+    }
+   
+    
+    // create span to put button inside him
     var spn=document.createElement("span");
     spn.appendChild(btn);
-    spn.style.position="absolute";
-    spn.style.marginTop="-40px";
-    spn.style.marginLeft="420px";
+
     var element = document.getElementById("todotask");
-    element.appendChild(para);
-    element.appendChild(spn);
- }
-    
+    element.appendChild(div);
+    div.appendChild(para);
+    spn.appendChild(checkbox);
+    div.appendChild(spn);
+ }    
 }
